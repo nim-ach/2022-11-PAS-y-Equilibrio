96722fe464418ae232c1c365f827f22f77d94a30
Matías Castillo <matcasti@umag.cl>
2022-11-13 19:17:41 -0300

    Separate files for each analysis type

---
+++ R/ordered_logistic_regression.R
@@ -0,0 +1,82 @@
+
+# load libraries and data ---------------------------------------------------------------------
+
+library(data.table)
+
+data("riskfall")
+
+# Explore dataset -----------------------------------------------------------------------------
+
+riskfall[, posturography_cat_ec := factor(posturography_cat_ec)][]
+
+ordinal_model <- MASS::polr(
+  formula = posturography_cat_ec ~ hsps_score, 
+  data = riskfall, 
+  Hess = TRUE)
+
+model_significance <- ordinal_model |> 
+  summary() |> 
+  coefficients() |> 
+  as.data.frame()
+
+model_significance$p_value <-  pnorm(model_significance[,"t value"]) * 2
+
+print(model_significance)
+
+predictor <- seq(
+  from = min(riskfall$hsps_score, na.rm = TRUE),
+  to = max(riskfall$hsps_score, na.rm = TRUE),
+  by = 0.05
+)
+
+probs <- marginaleffects::predictions(ordinal_model,
+        newdata = data.frame(hsps_score = predictor),
+        conf_level = .95,
+        type = "probs") |> 
+  data.table()
+
+
+# Visualize models --------------------------------------------------------
+
+.plot = FALSE
+
+if (.plot) {
+
+  ## Load libraries
+  
+  library(ggplot2)
+  
+  ## Conditional probability
+  
+  ggplot(probs, aes(hsps_score, predicted, fill = group)) +
+    facet_grid(cols = vars(group)) +
+    geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .3) +
+    geom_line(aes(col = group)) +
+    scale_y_continuous(expand = c(0,0)) +
+    scale_x_continuous(expand = c(0,0)) +
+    ggsci::scale_color_d3() +
+    ggsci::scale_fill_d3() +
+    labs(x = "HSPS Score", y = "P( Category | HSPS Score )",
+         fill = "Category", col = "Category",
+         title = "Postural instability and HSPS Score",
+         subtitle = paste(strwrap("Probability of being in a certain category of postural stability on the eyes closed assessment for a given HSPS score.", 80), collapse = "\n")) +
+    ggdist::theme_ggdist()
+  
+  ## Cummulative probability
+  
+  ggplot(probs, aes(hsps_score, predicted, fill = factor(group))) +
+    # facet_grid(cols = vars(group)) +
+    geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .3, position = "fill") +
+    # geom_line(aes(col = group)) +
+    scale_y_continuous(expand = c(0,0)) +
+    scale_x_continuous(expand = c(0,0)) +
+    ggsci::scale_color_d3() +
+    ggsci::scale_fill_d3() +
+    labs(x = "HSPS Score", y = "P( Category | HSPS Score )",
+         fill = "Category", col = "Category",
+         title = "Postural instability and HSPS Score",
+         subtitle = paste(strwrap("Probability of being in a certain category of postural stability on the eyes closed assessment for a given HSPS score.", 80), collapse = "\n")) +
+    ggdist::theme_ggdist()
+  
+}
+
